b03cd232c06689fab905d2a5bb6129f5173b595d
Shamshura Egor <shamshuraegor@gmail.com>
2026-02-25 07:57:21 +0000

    [CMake] Added build-presets.
    [Sim] Removed extra function call.
    
    [CMake] Fixed install. Fixed bundle install gems.
    
    [CMake] Fixed new line.

--- cmake/Bundler.cmake
+++ cmake/Bundler.cmake
@@ -3,6 +3,11 @@ if (NOT DEFINED BUNDLE_PATH)
 endif()
 
 execute_process(
+  COMMAND ${BUNDLE_PATH} config set --local path ${CMAKE_BINARY_DIR}/bundle
+  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+)
+
+execute_process(
   COMMAND ${BUNDLE_PATH} install
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 )
