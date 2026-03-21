02b8203d87e3a0b0563a79df26b41bddc7a07452
Shamshura Egor <shamshuraegor@gmail.com>
2026-03-21 04:18:05 +0000

    [CMake] Small fixes.

--- ci-extra/test.sh
+++ ci-extra/test.sh
@@ -1,4 +1,8 @@
 PRESET_NAME=$1
 
+# Configure CMake
+cmake -S . \
+  --preset "${PRESET_NAME}" -DPROTEA_BUILD_TESTS=true -DQEMU_PATH=qemu-riscv32 -G Ninja
+
 # Run tests
 cmake --build --preset ${PRESET_NAME} --target simtests
