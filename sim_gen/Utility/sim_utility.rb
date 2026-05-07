233adceb0c5ba2a29b1a3047e122aba07fef4bdb
Shamshura Egor <shamshuraegor@gmail.com>
2026-05-07 12:28:43 +0000

    [ADL, SimGen, Test] Remove xlen field. Small cmake improvement

--- sim_gen/Utility/sim_utility.rb
+++ sim_gen/Utility/sim_utility.rb
@@ -2,7 +2,7 @@ module SimGen
   module Helper
     module_function
 
-      def find_max_xlen(regfiles)
+      def find_max_regsize(regfiles)
         max_xlen = 0
         regfiles.each do |regfile|
           regfile[:regs].each do |reg|
@@ -11,5 +11,15 @@ module SimGen
         end
         max_xlen
       end
+
+      def find_max_insn_len(instructions)
+        max_len = 0
+        instructions.each do |insn|
+          insn[:code][:tree].each do |node|
+            len_insn = node[:fields].map { |f| f[:from] - f[:to] }.sum / 8
+            max_len = [max_len, len_insn].max
+          end
+        end
+        max_len
   end
 end
