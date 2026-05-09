fe18340a90c96927180771705e92c080673d1172
Shamshura Egor <shamshuraegor@gmail.com>
2026-05-09 10:29:32 +0000

    [SimTests] Clang-format

--- test/SimTests/C/CImpl/xorshift32/main.c
+++ test/SimTests/C/CImpl/xorshift32/main.c
@@ -6,28 +6,26 @@ constexpr uint32_t N = 1024 * 1024;
 uint32_t buffer[N];
 
 uint32_t xorshift32(uint32_t x) {
-    x ^= x << 13;
-    x ^= x >> 17;
-    x ^= x << 5;
-    return x;
+  x ^= x << 13;
+  x ^= x >> 17;
+  x ^= x << 5;
+  return x;
 }
 
 uint32_t run() {
-    uint32_t x = seed;
+  uint32_t x = seed;
 
-    for (uint32_t i = 0; i < N; ++i) {
-        x = xorshift32(x);
-        buffer[i] = x;
-    }
+  for (uint32_t i = 0; i < N; ++i) {
+    x = xorshift32(x);
+    buffer[i] = x;
+  }
 
-    uint32_t sum = 0;
+  uint32_t sum = 0;
 
-    for (uint32_t i = 0; i < N; ++i)
-        sum ^= buffer[i];
+  for (uint32_t i = 0; i < N; ++i)
+    sum ^= buffer[i];
 
-    return sum;
+  return sum;
 }
 
-int main() {
-    return run() == 752068848 ? 0 : 1;
-}
+int main() { return run() == 752068848 ? 0 : 1; }
