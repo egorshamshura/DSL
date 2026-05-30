326e6bfa5846da9351345ad682129d95cef7891e
Shamshura Egor <shamshuraegor@gmail.com>
2026-05-30 11:53:10 +0000

    [ADL] Decrease size of descri[tion

--- lib/Target/RISC-V/32I.rb
+++ lib/Target/RISC-V/32I.rb
@@ -1,292 +1,95 @@
 require_relative "encoding"
-require_relative "../../ADL/base"
-require_relative "../../ADL/builder"
+require "ADL/base"
+require "ADL/builder"
 
 module RV32I
-    include SimInfra
-    extend SimInfra
+  include SimInfra
+  extend SimInfra
 
-    Interface {
-        Function(:sysCall)
-    }
+  Interface { Function(:sysCall) }
 
-    RegisterFile(:XRegs) {
-        r32  :x0, zero
-        r32  :x1
-        r32  :x2, sp
-        r32  :x3
-        r32  :x4
-        r32  :x5
-        r32  :x6
-        r32  :x7
-        r32  :x8
-        r32  :x9
-        r32 :x10
-        r32 :x11
-        r32 :x12
-        r32 :x13
-        r32 :x14
-        r32 :x15
-        r32 :x16
-        r32 :x17
-        r32 :x18
-        r32 :x19
-        r32 :x20
-        r32 :x21
-        r32 :x22
-        r32 :x23
-        r32 :x24
-        r32 :x25
-        r32 :x26
-        r32 :x27
-        r32 :x28
-        r32 :x29
-        r32 :x30
-        r32 :x31
-        r32  :pc, pc
-    }
+  RegisterFile(:XRegs) {
+    r32 :x0, zero
+    r32 :x1
+    r32 :x2, sp
+    for i in 3..31 do; r32 :"x#{i}"; end
+    r32 :pc, pc
+  }
 
-    Instruction(:lui) {
-        encoding *format_u(0b0110111)
-        asm { "lui %{rd}, %{imm}" }
-        code { rd[]= imm }
-    }
+  def self.build_from_table(table)
+    table.each do |name, encoding_args, asm_str, code_str|
+      Instruction(name) {
+        encoding *encoding_args
+        asm { "#{name} #{asm_str}" }
+        code { instance_eval(code_str) }
+      }
+    end
+  end
 
-    Instruction(:auipc) {
-        encoding *format_u(0b0010111)
-        asm { "auipc %{rd}, %{imm}" }
-        code { rd[]= imm + pc }
-    }
+  INSTRUCTION_TABLE = [
 
-    Instruction(:add) {
-        encoding *format_r(0b0110011, 0b000, 0b0000000)
-        asm { "add %{rd}, %{rs1}, %{rs2}" }
-        code { rd[]= rs1.u + rs2.u }
-    }
+    [:add,  format_r(0b0110011,0b000,0b0000000), "%{rd}, %{rs1}, %{rs2}", "rd[]= rs1.u + rs2.u"],
+    [:sub,  format_r(0b0110011,0b000,0b0100000), "%{rd}, %{rs1}, %{rs2}", "rd[]= rs1.u - rs2.u"],
+    [:sll,  format_r(0b0110011,0b001,0b0000000), "%{rd}, %{rs1}, %{rs2}", "rd[]= rs1.u << rs2.u"],
+    [:slt,  format_r(0b0110011,0b010,0b0000000), "%{rd}, %{rs1}, %{rs2}", "rd[]= (rs1.s < rs2.s).b32"],
+    [:sltu, format_r(0b0110011,0b011,0b0000000), "%{rd}, %{rs1}, %{rs2}", "rd[]= (rs1.u < rs2.u).b32"],
+    [:xor,  format_r(0b0110011,0b100,0b0000000), "%{rd}, %{rs1}, %{rs2}", "rd[]= rs1 ^ rs2"],
+    [:srl,  format_r(0b0110011,0b101,0b0000000), "%{rd}, %{rs1}, %{rs2}", "rd[]= rs1.u >> rs2.u"],
+    [:sra,  format_r(0b0110011,0b101,0b0100000), "%{rd}, %{rs1}, %{rs2}", "rd[]= rs1.s >> rs2.s"],
+    [:or,   format_r(0b0110011,0b110,0b0000000), "%{rd}, %{rs1}, %{rs2}", "rd[]= rs1 | rs2"],
+    [:and,  format_r(0b0110011,0b111,0b0000000), "%{rd}, %{rs1}, %{rs2}", "rd[]= rs1 & rs2"],
 
-    Instruction(:sub) {
-        encoding *format_r(0b0110011, 0b000, 0b0100000)
-        asm { "sub %{rd}, %{rs1}, %{rs2}" }
-        code { rd[]= rs1.u - rs2.u }
-    }
+    [:addi,  format_i(0b0010011,0b000), "%{rd}, %{rs1}, %{imm}", "rd[]= rs1 + imm"],
+    [:slti,  format_i(0b0010011,0b010), "%{rd}, %{rs1}, %{imm}", "rd[]= (rs1.s < imm.s).b32"],
+    [:sltiu, format_i(0b0010011,0b011), "%{rd}, %{rs1}, %{imm}", "rd[]= (rs1.u < imm.u).b32"],
+    [:xori,  format_i(0b0010011,0b100), "%{rd}, %{rs1}, %{imm}", "rd[]= rs1 ^ imm"],
+    [:ori,   format_i(0b0010011,0b110), "%{rd}, %{rs1}, %{imm}", "rd[]= rs1 | imm"],
+    [:andi,  format_i(0b0010011,0b111), "%{rd}, %{rs1}, %{imm}", "rd[]= rs1 & imm"],
+    [:slli,  format_i_shift(0b0010011,0b001,0b00000), "%{rd}, %{rs1}, %{imm}", "rd[]= rs1 << imm"],
+    [:srli,  format_i_shift(0b0010011,0b101,0b00000), "%{rd}, %{rs1}, %{imm}", "rd[]= rs1.u >> imm"],
+    [:srai,  format_i_shift(0b0010011,0b101,0b01000), "%{rd}, %{rs1}, %{imm}", "rd[]= rs1.s >> imm"],
 
-    Instruction(:sll) {
-        encoding *format_r(0b0110011, 0b001, 0b0000000)
-        asm { "sll %{rd}, %{rs1}, %{rs2}" }
-        code { rd[]= rs1.u << rs2.u }
-    }
+    [:beq,  format_b(0b1100011,0b000), "%{rs1}, %{rs2}, %{imm}", "branch(select(rs1 == rs2, pc + imm, pc + 4))"],
+    [:bne,  format_b(0b1100011,0b001), "%{rs1}, %{rs2}, %{imm}", "branch(select(rs1 != rs2, pc + imm, pc + 4))"],
+    [:blt,  format_b(0b1100011,0b100), "%{rs1}, %{rs2}, %{imm}", "branch(select(rs1.s < rs2.s, pc + imm, pc + 4))"],
+    [:bge,  format_b(0b1100011,0b101), "%{rs1}, %{rs2}, %{imm}", "branch(select(rs1.s >= rs2.s, pc + imm, pc + 4))"],
+    [:bltu, format_b(0b1100011,0b110), "%{rs1}, %{rs2}, %{imm}", "branch(select(rs1.u < rs2.u, pc + imm, pc + 4))"],
+    [:bgeu, format_b(0b1100011,0b111), "%{rs1}, %{rs2}, %{imm}", "branch(select(rs1.u >= rs2.u, pc + imm, pc + 4))"],
 
-    Instruction(:slt) {
-        encoding *format_r(0b0110011, 0b010, 0b0000000)
-        asm { "slt %{rd}, %{rs1}, %{rs2}" }
-        code { rd[]= (rs1.s < rs2.s).b32 }
-    }
+    [:sb, format_s(0b0100011,0b000), "%{rs2}, %{imm}(%{rs1})", "mem[rs1 + imm]= rs2[7,0]"],
+    [:sh, format_s(0b0100011,0b001), "%{rs2}, %{imm}(%{rs1})", "mem[rs1 + imm]= rs2[15,0]"],
+    [:sw, format_s(0b0100011,0b010), "%{rs2}, %{imm}(%{rs1})", "mem[rs1 + imm]= rs2"],
 
-    Instruction(:sltu) {
-        encoding *format_r(0b0110011, 0b011, 0b0000000)
-        asm { "sltu %{rd}, %{rs1}, %{rs2}" }
-        code { rd[]= (rs1.u < rs2.u).b32 }
-    }
+    [:lb,  format_i(0b0000011,0b000), "%{rd}, %{imm}(%{rs1})", "rd[]= mem[rs1 + imm, :b8].s32"],
+    [:lh,  format_i(0b0000011,0b001), "%{rd}, %{imm}(%{rs1})", "rd[]= mem[rs1 + imm, :b16].s32"],
+    [:lw,  format_i(0b0000011,0b010), "%{rd}, %{imm}(%{rs1})", "rd[]= mem[rs1 + imm, :b32]"],
+    [:lbu, format_i(0b0000011,0b100), "%{rd}, %{imm}(%{rs1})", "rd[]= mem[rs1 + imm, :b8].u32"],
+    [:lhu, format_i(0b0000011,0b101), "%{rd}, %{imm}(%{rs1})", "rd[]= mem[rs1 + imm, :b16].u32"]
+  ]
 
-    Instruction(:xor) {
-        encoding *format_r(0b0110011, 0b100, 0b0000000)
-        asm { "xor %{rd}, %{rs1}, %{rs2}" }
-        code { rd[]= rs1 ^ rs2 }
-    }
+  build_from_table(INSTRUCTION_TABLE)
 
-    Instruction(:srl) {
-        encoding *format_r(0b0110011, 0b101, 0b0000000)
-        asm { "srl %{rd}, %{rs1}, %{rs2}" }
-        code { rd[]= rs1.u >> rs2.u }
-    }
+  Instruction(:lui) {
+    encoding *format_u(0b0110111)
+    asm { "lui %{rd}, %{imm}" }
+    code { rd[]= imm }
+  }
 
-    Instruction(:sra) {
-        encoding *format_r(0b0110011, 0b101, 0b0100000)
-        asm { "sra %{rd}, %{rs1}, %{rs2}" }
-        code { rd[]= rs1.s >> rs2.s }
-    }
+  Instruction(:auipc) {
+    encoding *format_u(0b0010111)
+    asm { "auipc %{rd}, %{imm}" }
+    code { rd[]= imm + pc }
+  }
 
-    Instruction(:or) {
-        encoding *format_r(0b0110011, 0b110, 0b0000000)
-        asm { "or %{rd}, %{rs1}, %{rs2}" }
-        code { rd[]= rs1 | rs2 }
-    }
+  Instruction(:jal) {
+    encoding *format_j(0b1101111)
+    asm { "jal %{rd}, %{imm}" }
+    code { rd[]= pc + 4; branch(pc + imm) }
+  }
 
-    Instruction(:and) {
-        encoding *format_r(0b0110011, 0b111, 0b0000000)
-        asm { "and %{rd}, %{rs1}, %{rs2}" }
-        code { rd[]= rs1 & rs2 }
-    }
-
-    Instruction(:addi) {
-        encoding *format_i(0b0010011, 0b000)
-        asm { "addi %{rd}, %{rs1}, %{imm}" }
-        code { rd[]= rs1 + imm }
-    }
-
-    Instruction(:slti) {
-        encoding *format_i(0b0010011, 0b010)
-        asm { "slti %{rd}, %{rs1}, %{imm}" }
-        code { rd[]= (rs1.s < imm.s).b32  }
-    }
-
-    Instruction(:sltiu) {
-        encoding *format_i(0b0010011, 0b011)
-        asm { "sltiu %{rd}, %{rs1}, %{imm}" }
-        code { rd[]= (rs1.u < imm.u).b32 }
-    }
-
-    Instruction(:xori) {
-        encoding *format_i(0b0010011, 0b100)
-        asm { "xori %{rd}, %{rs1}, %{imm}" }
-        code { rd[]= rs1 ^ imm }
-    }
-
-    Instruction(:ori) {
-        encoding *format_i(0b0010011, 0b110)
-        asm { "ori %{rd}, %{rs1}, %{imm}" }
-        code { rd[]= rs1 | imm }
-    }
-
-    Instruction(:andi) {
-        encoding *format_i(0b0010011, 0b111)
-        asm { "andi %{rd}, %{rs1}, %{imm}" }
-        code { rd[]= rs1 & imm }
-    }
-
-    Instruction(:slli) {
-        encoding *format_i_shift(0b0010011, 0b001, 0b00000)
-        asm { "slli %{rd}, %{rs1}, %{imm}" }
-        code { rd[]= rs1 << imm }
-    }
-
-    Instruction(:srli) {
-        encoding *format_i_shift(0b0010011, 0b101, 0b00000)
-        asm { "srli %{rd}, %{rs1}, %{imm}" }
-        code { rd[]= rs1 >> imm }
-    }
-
-    Instruction(:srai) {
-        encoding *format_i_shift(0b0010011, 0b101, 0b01000)
-        asm { "srai %{rd}, %{rs1}, %{imm}" }
-        code { rd[]= rs1.s >> imm }
-    }
-
-    Instruction(:beq) {
-        encoding *format_b(0b1100011, 0b000)
-        asm { "beq %{rs1}, %{rs2}, %{imm}" }
-        code { branch(select(rs1 == rs2, pc + imm, pc + 4)) }
-    }
-
-    Instruction(:bne) {
-        encoding *format_b(0b1100011, 0b001)
-        asm { "bne %{rs1}, %{rs2}, %{imm}" }
-        code { branch(select(rs1 != rs2, pc + imm, pc + 4)) }
-    }
-
-    Instruction(:blt) {
-        encoding *format_b(0b1100011, 0b100)
-        asm { "blt %{rs1}, %{rs2}, %{imm}" }
-        code { branch(select(rs1.s < rs2.s, pc + imm, pc + 4)) }
-    }
-
-    Instruction(:bge) {
-        encoding *format_b(0b1100011, 0b101)
-        asm { "bge %{rs1}, %{rs2}, %{imm}" }
-        code { branch(select(rs1.s >= rs2.s, pc + imm, pc + 4)) }
-    }
-
-    Instruction(:bltu) {
-        encoding *format_b(0b1100011, 0b110)
-        asm { "bltu %{rs1}, %{rs2}, %{imm}" }
-        code { branch(select(rs1.u < rs2.u, pc + imm, pc + 4)) }
-    }
-
-    Instruction(:bgeu) {
-        encoding *format_b(0b1100011, 0b111)
-        asm { "bgeu %{rs1}, %{rs2}, %{imm}" }
-        code { branch(select(rs1.u >= rs2.u, pc + imm, pc + 4)) }
-    }
-
-    Instruction(:jal) {
-        encoding *format_j(0b1101111)
-        asm { "jal %{rd}, %{imm}" }
-        code { rd[]= pc + 4; branch(pc + imm) }
-    }
-
-    Instruction(:jalr) {
-        encoding *format_i(0b1100111, 0b000)
-        asm { "jalr %{rd}, %{rs1}, %{imm}" }
-        code { 
-          let :t, :b32, pc + 4
-          branch((rs1 + imm) & (~1))
-          rd[]= t
-        }
-    }
-
-    Instruction(:sb) {
-        encoding *format_s(0b0100011, 0b000)
-        asm { "sb %{rs2}, %{imm}(%{rs1})" }
-        code { mem[rs1 + imm]= rs2[7, 0] }
-    }
-
-    Instruction(:sh) {
-        encoding *format_s(0b0100011, 0b001)
-        asm { "sh %{rs2}, %{imm}(%{rs1})" }
-        code { mem[rs1 + imm]= rs2[15, 0] }
-    }
-
-    Instruction(:sw) {
-        encoding *format_s(0b0100011, 0b010)
-        asm { "sw %{rs2}, %{imm}(%{rs1})" }
-        code { mem[rs1 + imm]= rs2 }
-    }
-
-    Instruction(:lb) {
-        encoding *format_i(0b0000011, 0b000)
-        asm { "lb %{rd}, %{imm}(%{rs1})" }
-        code { rd[]= mem[rs1 + imm, :b8].s32 }
-    }
-
-    Instruction(:lh) {
-        encoding *format_i(0b0000011, 0b001)
-        asm { "lh %{rd}, %{imm}(%{rs1})" }
-        code { rd[]= mem[rs1 + imm, :b16].s32 }
-    }
-
-    Instruction(:lw) {
-        encoding *format_i(0b0000011, 0b010)
-        asm { "lw %{rd}, %{imm}(%{rs1})" }
-        code { rd[]= mem[rs1 + imm, :b32] }
-    }
-    
-    Instruction(:lbu) {
-        encoding *format_i(0b0000011, 0b100)
-        asm { "lbu %{rd}, %{imm}(%{rs1})" }
-        code { rd[]= mem[rs1 + imm, :b8].u32 }
-    }
-
-    Instruction(:lhu) {
-        encoding *format_i(0b0000011, 0b101)
-        asm { "lhu %{rd}, %{imm}(%{rs1})" }
-        code { rd[]= mem[rs1 + imm, :b16].u32 }
-    }
-
-    Instruction(:ecall) {
-        encoding :E, [field(:c, 31, 0, 0b1110011)]
-        asm { "ecall" }
-        code { sysCall }
-    }
-
-    Instruction(:ebreak) {
-        encoding :E, [field(:c, 31, 0, 0b100000000000001110011)]
-        asm { "ebreak" }
-        code { }
-    }
-
-    Instruction(:fence) {
-        encoding :E, [field(:c1, 31, 28, 0b0000), field(:c2, 27, 24), field(:c3, 23, 20), field(:c4, 19, 0, 0b00000000000000001111)]
-        asm { "fence" }
-        code { }
-    }
+  Instruction(:jalr) {
+    encoding *format_i(0b1100111, 0b000)
+    asm { "jalr %{rd}, %{rs1}, %{imm}" }
+    code { let :t, :b32, pc + 4; branch((rs1 + imm) & (~1)); rd[]= t }
+  }
 end
--- lib/Target/RISC-V/encoding.rb
+++ lib/Target/RISC-V/encoding.rb
@@ -1,4 +1,4 @@
-require_relative "../../ADL/base"
+require "ADL/base"
 
 module SimInfra
     def u_imm(imm)
@@ -33,77 +33,52 @@ end
 module SimInfra
     def format_u(opcode)
         return :U, [
-            field(:f_opcode, 6, 0, opcode),
-            field(:f_rd, 11, 7),
-            field(:f_imm, 31, 12),
+            field(:f_opcode, 6, 0, opcode), field(:f_rd, 11, 7), field(:f_imm, 31, 12),
         ], xreg(:rd), u_imm(:imm)
     end
 
     def format_r(opcode, funct3, funct7)
         return :R, [
-            field(:f_opcode, 6, 0, opcode),
-            field(:f_rd, 11, 7),
-            field(:f_funct3, 14, 12, funct3),
-            field(:f_rs1, 19, 15),
-            field(:f_rs2, 24, 20),
-            field(:f_funct7, 31, 25, funct7),
+            field(:f_opcode, 6, 0, opcode), field(:f_rd, 11, 7),
+            field(:f_funct3, 14, 12, funct3), field(:f_rs1, 19, 15),
+            field(:f_rs2, 24, 20), field(:f_funct7, 31, 25, funct7),
         ], xreg(:rs2), xreg(:rs1), xreg(:rd)
     end
 
     def format_i(opcode, funct3)
         return :I, [
-            field(:f_opcode, 6, 0, opcode),
-            field(:f_rd, 11, 7),
-            field(:f_funct3, 14, 12, funct3),
-            field(:f_rs1, 19, 15),
-            field(:f_imm, 31, 20),
+            field(:f_opcode, 6, 0, opcode), field(:f_rd, 11, 7), field(:f_funct3, 14, 12, funct3),
+            field(:f_rs1, 19, 15), field(:f_imm, 31, 20),
         ], i_imm(:imm), xreg(:rs1), xreg(:rd)
     end
 
     def format_i_shift(opcode, func3, sopcode)
         return :srai, [
-            field(:f_opcode, 6, 0, opcode),
-            field(:func3, 14, 12, func3),
-            field(:f_imm4_0, 24, 20),
-            field(:f_rd, 11, 7),
-            field(:f_rs1, 19, 15),
-            field(:f_temp, 26, 25, 0b01),
+            field(:f_opcode, 6, 0, opcode), field(:func3, 14, 12, func3), field(:f_imm4_0, 24, 20),
+            field(:f_rd, 11, 7), field(:f_rs1, 19, 15), field(:f_temp, 26, 25, 0b01),
             field(:f_sopcode, 31, 27, sopcode),
         ], is_imm(:imm), xreg(:rs1), xreg(:rd)
     end
 
     def format_b(opcode, funct3)
         return :B, [
-            field(:f_opcode, 6, 0, opcode),
-            field(:f_funct3, 14, 12, funct3),
-            field(:f_rs1, 19, 15),
-            field(:f_rs2, 24, 20),
-            field(:f_imm4_1, 11, 8),
-            field(:f_imm10_5, 30, 25),
-            field(:f_imm11, 7, 7),
-            field(:f_imm12, 31, 31),
+            field(:f_opcode, 6, 0, opcode), field(:f_funct3, 14, 12, funct3), field(:f_rs1, 19, 15),
+            field(:f_rs2, 24, 20), field(:f_imm4_1, 11, 8), field(:f_imm10_5, 30, 25),
+            field(:f_imm11, 7, 7), field(:f_imm12, 31, 31),
         ], b_imm(:imm), xreg(:rs1), xreg(:rs2)
     end
 
     def format_j(opcode)
         return :J, [
-            field(:f_opcode, 6, 0, opcode),
-            field(:f_rd, 11, 7),
-            field(:f_imm20, 31, 31),
-            field(:f_imm19_12, 19, 12),
-            field(:f_imm11, 20, 20),
-            field(:f_imm10_1, 30, 21),
+            field(:f_opcode, 6, 0, opcode), field(:f_rd, 11, 7), field(:f_imm20, 31, 31),
+            field(:f_imm19_12, 19, 12), field(:f_imm11, 20, 20), field(:f_imm10_1, 30, 21),
         ], j_imm(:imm), xreg(:rd)
     end
 
     def format_s(opcode, func3)
         return :S, [
-            field(:f_opcode, 6, 0, opcode),
-            field(:func3, 14, 12, func3),
-            field(:f_imm4_0, 11, 7),
-            field(:f_rs1, 19, 15),
-            field(:f_rs2, 24, 20),
-            field(:f_imm11_5, 31, 25),
+            field(:f_opcode, 6, 0, opcode), field(:func3, 14, 12, func3), field(:f_imm4_0, 11, 7),
+            field(:f_rs1, 19, 15), field(:f_rs2, 24, 20), field(:f_imm11_5, 31, 25),
         ], s_imm(:imm), xreg(:rs1), xreg(:rs2)
     end
 end
